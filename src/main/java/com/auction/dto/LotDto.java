2f0532b5f0ec986abdddecc9f36f550a3b1891dc
Vladimir Fomin <Vladimir_Fomin@epam.com>
2022-04-01 17:46:46 +0300

    AU-5 - VF - Add lot entity and lot dto

---
+++ src/main/java/com/auction/dto/LotDto.java
@@ -0,0 +1,52 @@
+package com.auction.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.time.LocalDateTime;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@EqualsAndHashCode
+public class LotDto {
+
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
+    private Long id;
+
+    @NotBlank(message = "Please provide a name for the Present")
+    @Size(min = 1, max = 60, message = "The Present Name needs to be between 1 and 60 characters "
+                                       + "long and contain cyrillic symbols, latin symbols and "
+                                       + "numbers.")
+    private String name;
+
+
+    /*
+    The character limit on the backend includes text formatting from the front
+    and this reduces the payload (800 characters), so we need to increase the
+    allowed limit on the backend to take this design into account.
+     */
+    @Size(min = 1, max = 1600, message = "The Description field needs to be between 1 and 1600 "
+                                         + "characters long and contain cyrillic symbols, latin "
+                                         + "symbols, numbers and special symbols")
+    private String description;
+
+    @Min(value = 0, message = "Invalid Bid. The field must contain only a positive integer.")
+    @Max(value = Long.MAX_VALUE)
+    private Long startBid;
+
+    @Min(value = 1, message = "Invalid Max bid rate step. The field must contain only a positive "
+                              + "integer.")
+    @Max(value = Integer.MAX_VALUE)
+    private Integer maxBidRateStep;
+
+    private LocalDateTime localDateTime;
+}
---
+++ src/main/java/com/auction/entity/Lot.java
@@ -0,0 +1,40 @@
+package com.auction.entity;
+
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "lot")
+public class Lot {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title", nullable = false, length = 50)
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "start_price")
+    private Long startBid;
+
+    @Column(name = "start_time")
+    private LocalDateTime startDateTime;
+
+    @Column(name = "end_time")
+    private LocalDateTime endDateTime;
+}
