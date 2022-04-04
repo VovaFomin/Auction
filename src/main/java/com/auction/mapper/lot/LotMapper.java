93af1ef1d7fa510880bdc29d9a0243e5cee10b3f
Vladimir Fomin <Vladimir_Fomin@epam.com>
2022-04-04 14:42:21 +0300

    AU-6 - VF - Add lot mapper

---
+++ src/main/java/com/auction/mapper/lot/LotMapper.java
@@ -0,0 +1,21 @@
+package com.auction.mapper.lot;
+
+import com.auction.dto.LotDto;
+import com.auction.entity.Lot;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface LotMapper {
+
+    LotMapper instance = Mappers.getMapper(LotMapper.class);
+
+    @Mapping(source = "lotDateTime.startDateTime", target = "startDateTime")
+    @Mapping(source = "lotDateTime.endDateTime", target = "endDateTime")
+    Lot convert(LotDto lotDto);
+
+    @Mapping(source = "startDateTime", target = "lotDateTime.startDateTime")
+    @Mapping(source = "endDateTime", target = "lotDateTime.endDateTime")
+    LotDto convert(Lot lot);
+}
